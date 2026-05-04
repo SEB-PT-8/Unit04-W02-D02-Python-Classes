9dc003124872120c507944a2c8342c15ada3015d
omarakamal <omaraakamal@gmail.com>
2026-05-04 19:06:42 +0300

    update

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@ print('Classes Lesson')
 class BankAccount():
     # 1. all classes have a __init__() method
     # 2. ALL methods have first parameter self
-    def __init__(self, balance_param, owner_param):
+    def __init__(self, owner_param, balance_param = 100):
         print('init function called')
         self.balance = balance_param
         self.owner = owner_param
@@ -15,13 +15,18 @@ class BankAccount():
 
 
 # create an object of the BankAccount class
-ali_account = BankAccount(500, 'Ali')
+ali_account = BankAccount('Ali',500)
 
-omar_account = BankAccount(200, 'Omar' )
+omar_account = BankAccount('Omar',200 )
+
+mohammad_account = BankAccount('Mohammad')
 
 omar_account.withdraw(150)
 
 print(omar_account.balance)
 
 print(ali_account.balance)
+
+print(mohammad_account.balance)
+
 
